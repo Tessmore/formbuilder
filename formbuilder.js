81e3aa1642d87190b33e4037185533cb47bcc69c
Fabien Tesselaar <fabientesselaar@gmail.com>
2013-07-15 04:26:14 +0200

    Increased performance

--- formbuilder.js
+++ formbuilder.js
@@ -1,102 +1,215 @@
 // Author: Fabien Tesselaar (https://github.com/Tessmore/formbuilder)
 
-$.fn.formbuilder = function() {  
+(function($, window, undefined){
+  $.fn.formbuilder = function(opts) {
 
-  var textarea = $('<textarea />');
-  var result   = $('<pre />');
-  var form     = $('<form />');
-  var group    = $('<div class="control-group" />');
-  var controls = $('<div class="controls" />');
-  var label    = $('<label class="control-label" />');
-  
-  var fields = {
-    'text'     : $('<input type="text" name id>'),
-    'radio'    : $('<div />'),
-    'checkbox' : $('<div />'),
-    'textarea' : $('<textarea name id />'),
-    'select'   : $('<select name id></select>'),
-    'file'     : $('<input type="file" name id>')
-  };
+    var templates = $('#formbuilder-templates'); 
 
-  this
-    .append(textarea)
-    .after(result)
-    .after('<h3>Result</h3>')
-    .after(form)
-    .after('<h3>Preview</h3>');
+    var defaults = {
+      'throttle'       : 500,
+      'separator'      : '|',
+      'args_separator' : ','
+    };
 
-  textarea.on('keyup', function() {
-    form.html(''); // Reset the control
-    var lines = this.value.split("\n");
-    
-    for (var i=0; i < lines.length; i++) {
-      if (lines[i].charAt(0) == '#') {
-        form.append('<h3>' + lines[i].substring(1) + '</h3>');
-        continue;
+    opts = $.extend(defaults, opts);
+
+    var textarea = $(this).find('#builder-input');
+    var result   = $(this).find('#result-html');
+    var form     = $(this).find('#form');
+ 
+    // Build the form on ENTER, losing focus or after certain periods of typing
+    textarea.on('keydown change', function(e) {
+      if (e.keyCode === 13 || e.type === 'change') {
+        window.clearInterval(timer);
+        build_form;
+        return;
       }
       
-      options = parseLine(lines[i]);
-      type    = options.type || options.t || "text";
+      var timer = setTimeout(build_form, opts.throttle);
+    });
       
-      if (! options || !fields[type])
-        continue;
+    var build_form = function build() {
+      form.html(''); // Reset
+      var lines = textarea.val().split("\n");
+      
+      for (var i=0; i < lines.length; i++) {
+        var line       = trim(lines[i], ' ');
+        var first_char = line.charAt(0);
+        var attributes = {};
+        var type       = '';
 
-      group
-        .append(label.text(options.label))
-        .append(
-          controls.html(
-            fields[type]
-              .attr('name', options.name)
-              .attr('id',   options.id)
-         ));
-      
-      if (type === 'select' || type === 'radio' || type === 'checkbox') {
-        fields[type].html(''); // Reset the list
-        
-        while (++i < lines.length && lines[i] && (lines[i].charAt(0) == '*' || lines[i].charAt(0) == '-')) {
-          value = lines[i].substring(1);
+        // Trim spaces; Skip empty lines
+        if (line === '')
+          continue;
           
-          if (type === 'select')
-            fields[type].append($('<option />').text(value));
-          else
-            fields[type].append(
-              '<label class="checkbox">' +
-                '<input type="' + type + '" name="' + (type === 'radio' ? options.name : value) + '" value="' + value + '"> ' + 
-                value + 
-              '</label>'
-            );
+        if (line.toLowerCase() === "weekend") {
+          lines[i] = '';
+          lines.splice(i, 0, 
+            'Dieet | checkbox', 
+            '-Vegetarisch', 
+            '-Veganistisch',
+            'shirt',
+            'Noodnummer',
+            '> Telefoon nummer in geval van nood',
+            'Allergie/medicatie | textarea',
+            '> Waar moeten we rekening mee houden'
+          );
+
+          textarea.val(lines.join("\n"));
+          textarea.trigger('keyup');
+          break;
+        }
+
+        if (line.toLowerCase() === "shirt") {
+          lines[i] = '';
+          lines.splice(i, 0, 
+            'Shirt maat | select',
+            '-Small',
+            '-Medium',
+            '-Large'
+          );
+
+          textarea.val(lines.join("\n"));
+          textarea.trigger('keyup');
+          break;
         }
         
-        i--; // Go back 1 step to parse the next element without #hashtag
-      }
-      
-      group.clone().appendTo(form);
-    }
-    
-    result.text(form.html());
-  });
-  
-  function parseLine(line) {
-    var line = line.trim().replace(/["']/g, '').split("-");
-    var name = line[0];
+        // Check first character; Add headline '#' or paragraph '$'
+        if (first_char === '#' || first_char === '$') {
+          add((first_char === '$' ? '<p>' : '<h3>') + trim(line, '#$ '));
+          continue;
+        }
 
-    if (name == "") // Skip empty lines
-      return false;
+        // Check first character; Add additional info after the label '>'
+        if (first_char === '>') {
+          form.find('label').last().after('<small>' + trim(line, '> ') + '</small>');
+          continue;
+        }
           
-    var options = {
-      label : name,
-      name  : name.replace(/[ ]/g, '_')
-    };
-    
-    if (line[1]) {
-      var args = line[1].replace(/[ ]/g, '').split(',');
-      
-      for (i in args) {
-        argument = args[i].split('=');
-        options[argument[0]] = argument[1];
+        // First character is not special; 
+        // Check the line is simple `input type="text"` field
+        if (line.indexOf(opts.separator) === -1) {
+          var label = line;
+          var field = templates.find('.text');
+          var input = field.find('input');
+        }
+        else {
+          // There are arguments; let's find them
+          var tmp   = line.split(opts.separator); 
+          var label = trim(tmp[0], ' ');
+          
+          if (tmp[1]) {
+            var args = tmp[1].replace(/["' ]/g, '').split(opts.args_separator);
+
+            for (arg in args) {
+              argument = args[arg].split('=');
+              
+              if (argument[0] === 'textarea' || 
+                  argument[0] === 'select'   || 
+                  argument[0] === 'radio'    || 
+                  argument[0] === 'checkbox') {
+                type = argument[0];
+              }
+              else if (argument[0] === 't' || argument[0] === 'type')
+                type = argument[1];
+              else
+                attributes[argument[0]] = argument[1];
+            }
+          }
+          
+          // If a type is found; lets build it
+          switch(type) {
+            case 'textarea' :
+              var field = templates.find('.textarea');
+              var input = field.find('textarea');
+            break;
+            
+            case 'select':
+              var field = templates.find('.select');
+              var input = field.find('select');
+              var options = [];
+              
+              while (++i < lines.length && lines[i].charAt(0) === '-')
+                options.push('<option>', trim(lines[i], '- '), '</option>');
+              
+              i--;
+              input.html(options.join(''));
+            break;
+            
+            case 'radio' :
+            case 'checkbox' :
+              // Since it uses additional labels, the radio / checkbox 
+              // continues earlier
+              
+              var field = templates.find('.box');
+                  field.find('label').text(label);
+              var input = field.find('.controls');
+              var name  = label.replace(/[ ]/g, '_') + '[]';
+              var options = [];
+
+              while (++i < lines.length && lines[i].charAt(0) == '-') {
+                var val = trim(lines[i], '- ');
+                
+                options.push(
+                  '<label class="checkbox">', 
+                    '<input type="'+ type +'" ', 
+                      'name="'+ name +'" ',
+                      'value="'+ val +'">',
+                    val,
+                  '</label>'
+                );
+              }
+                  
+              i--;
+              input.html(options.join(''));
+              
+              // Add the object to the form
+              add(field.clone());
+              continue;
+            break;
+            
+            default :
+              var field = templates.find('.text');
+              var input = field.find('input');
+            break;
+          }
+        }
+        
+        // Set 'standard' name, same as the label. Can be overridden
+        field.find('label').text(label);
+        input.attr('name', trim(label, '!@#$%.,:;').replace(/[ ]/g, '_'));
+        
+        // Add the attributes
+        for (attribute in attributes)
+          input.attr(attribute, attributes[attribute]);
+
+        // Add the object to the form
+        add(field.clone());
       }
+      
+      // Add the html in result if needed
+      result.text(form.html());
+    }
+    
+    function add(str) {
+      var tmp = form.append(str).html();
+      form.html(tmp + "\n\n");
+    }
+    
+    // Remove character set (string) from beginning and end of string
+    function trim(str, characters) {
+      var c_array = characters.split('');
+      var result  = '';
+      
+      for (var i=0; i < characters.length; i++)
+        result += '\\' + c_array[i];
+
+      return str.replace(
+        new RegExp('^['+ result +']+|['+ result +']+$', 'g'), 
+        ''
+      );
     }
 
-    return options;
-  }  
-};
+    return this;
+  };
+})(jQuery, window);
